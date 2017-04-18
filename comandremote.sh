2d4a25664110732f74b603c7230ac8ac9b9510bd
george0884 <georgecabrera08@gmail.com>
2017-04-18 02:20:35 -0300

    Update comandremote.sh

--- comandremote.sh
+++ comandremote.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect -f
-spawn ssh -p 22 root@insertyouip
+spawn ssh -p 22 username@xx.xxx.xx.xx
 expect "assword:"
-send "insertyoupass\r"
-send "insertcomand\r"
+send "insert_your_pass\r"
+send "insert_comand_wanted\r"
 set results $expect_out(buffer)
 #To end send command exit
 expect eof
