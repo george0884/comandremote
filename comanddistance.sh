7adbadc9592e692481776c6a161cc0d341508bac
george0884 <georgecabrera08@gmail.com>
2017-04-18 01:45:42 -0300

    Create comanddistance.sh
    To use this script you must execute ./comanddistance "command inside quotes" example ./comanddistance "ls -a"

---
+++ comanddistance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+HOST="you_ip_here"
+USER="user_name"
+PASS="You_pass_here"
+CMD=$@
+VAR=$(expect -c "
+spawn ssh -o StrictHostKeyChecking=no $USER@$HOST $CMD
+match_max 100000
+expect \"*?assword:*\"
+send -- \"$PASS\r\"
+send -- \"\r\"
+expect eof
+")
+echo "==============="
+echo "$VAR"
