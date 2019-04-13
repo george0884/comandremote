545e73ce78faf4835b742a51061befaf0cf57a72
The One Deluxe <georgecabrera08@gmail.com>
2019-04-13 18:11:28 -0300

    Update conectserver.sh

--- conectserver.sh
+++ conectserver.sh
@@ -3,3 +3,10 @@ spawn ssh -p 22 user@xx.xxx.xx.xx
 expect "assword:"
 send "insert_your_pass_here\r"
 interact
+
+#instrucciones
+#antes de usar el script instalar expect sudo apt install expect
+#Modificar linea 2 con la ip del servidor
+#Escribir pass en lenea 4 "insert_your_pass_here\r"
+#sudo chmod +x conectserver.sh
+#sudo ./conectserver.sh
